563de39fb1f3d516a80daa14b6e64c73767ae461
Thiago Mosqueiro <thiago.mosqueiro@gmail.com>
2015-05-14 20:47:02 -0300

    Integratino- Noise fixed

--- Medium-Projects/LeakyIntegratino/LeakyIntegratino.ino
+++ Medium-Projects/LeakyIntegratino/LeakyIntegratino.ino
@@ -2,29 +2,34 @@
 int t = 0;
 double dt = 0.01;
 
-double V0  = 0.0;
-double Vm  = 0.0;
-double Vth = 1.0;
-double Vr  = 0.0;
-double g   = 1.0;
-double PulseAmplitude = 5.0;
-double noiseAmplitude = .8;
-double R = 0;
+double V0     = 0.0; // resting potential
+double Vm     = 0.0; // membrane potential
+double Vth    = 1.0; // threshold potential
+double Vr     = 0.0; // resetting potential
+double g      = 1.0; //
+int inCurrent = 0;   // input current
+
+double PulseAmplitude = 5.0; // amplitude of the button pulse
+double noiseAmplitude = .8;  // noise amplitude
+double R = 0;                // noise variable
 
 // Spiked in the last t_rest period flag
-double t_rest = 0.5;
-double last_spike = -100.;
-boolean Not_Resting = true;
+double t_rest = 0.5;          // refractory period
+double last_spike = -100.;    // last spike timing
+boolean Not_Resting = true;   // refractory period flag
 
-int ledPin_spikesign = 5;
+
+// Digital Pins setup
+int ledPin_spikesign = 5;  // LED showing spikes
 int readInterrupt    = 2;  // Probing pin
 int ledPin_inputSPK  = 3;  // Output LED pin
-int inCurrent = 0;  // The input state
 
 // Temperature sensor
-int tempPin = 1;
-int reading;
-float NersntEffectiveCons = 0.05;
+int tempPin = 1;        // Analog temperature sensor pin
+int reading;            // Reading from sensor
+float NersntEffectCons = 0.05;
+
+
 
 // Setting up the serial port
 void setup() 
@@ -52,7 +57,7 @@ void loop()
   // Conversion to Kelvin
   float Temp = ( reading / 1024.0 )*500 + 273;
   // Updating resting potential
-  V0 = (Temp - 298)*NersntEffectiveCons;
+  V0 = (Temp - 298)*NersntEffectCons;
   
   // Reading from the button
   inCurrent = 1 - digitalRead(readInterrupt);
@@ -71,8 +76,8 @@ void loop()
   }
   else
   {
+    // Ensuring a refractory period
     Vm = Vr;
-    
     if ( t*dt - last_spike > t_rest )
     {
       Not_Resting = true;
@@ -89,12 +94,13 @@ void loop()
   }
   
   t++;
+  // Printing result through serial port
   Serial.print(t*dt, 2);
   Serial.print(" ");
   Serial.print(V0, 2);
   Serial.print(" ");
   Serial.println(Vm, 8);
   
-  delay(50);
+  delay(30);
 }
 
--- Medium-Projects/LeakyIntegratino/Readme.md
+++ Medium-Projects/LeakyIntegratino/Readme.md
@@ -5,6 +5,8 @@ The idea is to develop a leaky integrate-and-fire neuron module with
 temperature dependence and a current clamp.
 
 <img src="https://raw.githubusercontent.com/thmosqueiro/PlaygroundINO/master/Medium-Projects/LeakyIntegratino/LeakyIntegratino.png" width=400px style="display: block; margin: 10px auto 10px auto;" />
+<img src="https://raw.githubusercontent.com/thmosqueiro/PlaygroundINO/master/Medium-Projects/LeakyIntegratino/Temperature+Noise_effects.png" width=400px style="display: block; margin: 10px auto 10px auto;" />
+
 
 The current is controlled by a simple (four-legs) switch. It's
 amplitued is controlled via software and read through a digital
